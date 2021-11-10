e3aac06feede0ef495260d54bb6c15310db1a95c
Carlos Betiol <carlos@sistelpa.com>
2021-11-10 19:52:01 -0300

    implementada a classe model de agencia

--- src/main/java/com/banco/bluebank/model/Agencia.java
+++ src/main/java/com/banco/bluebank/model/Agencia.java
@@ -1,5 +1,67 @@
 package com.banco.bluebank.model;
 
-public class Agencia {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
 
+@Entity
+public class Agencia implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_agencia")
+    private int id;
+
+    private String agencia;
+
+    @Column(name="nome_agencia")
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public Agencia() {
+    }
+
+    public Agencia(int id, String agencia, String nome) {
+        this.id = id;
+        this.agencia = agencia;
+        this.nome = nome;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Agencia agencia = (Agencia) o;
+        return id == agencia.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
