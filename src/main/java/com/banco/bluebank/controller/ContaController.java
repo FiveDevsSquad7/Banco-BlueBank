643dd273eab970298f945e0a4eca774aa0f08b48
Weslley Lima <weslleyclima@yahoo.com.br>
2021-11-13 19:22:43 -0300

    Configuração do Swagger e juntamente codificação REST GENÃERICO

--- src/main/java/com/banco/bluebank/controller/ContaController.java
+++ src/main/java/com/banco/bluebank/controller/ContaController.java
@@ -1,5 +1,58 @@
 package com.banco.bluebank.controller;
 
+import com.banco.bluebank.model.Conta;
+import com.banco.bluebank.service.ContaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/conta")
 public class ContaController {
+
+    @Autowired
+    private ContaService service;
+
+    @GetMapping
+    public List<Conta> buscarTodos() {
+        return service.buscarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
+       Optional<Conta> conta = service.buscarId(id);
+        if(!conta.isEmpty()) {
+            return ResponseEntity.ok(conta.orElseThrow(() -> new RuntimeException("Não foi possível encontrar essa com por esse ID!")));
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Conta salvar(@RequestBody @Valid Conta contaInput) {
+        return service.salvar(contaInput);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> atualizar(@RequestBody Conta contaInput, @PathVariable Long id){
+        Optional<Conta> optionalConta = Optional.ofNullable(service.buscarPorIdParaAtualizar(id));
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> excluir(@PathVariable Long id) {
+        Optional<Conta> optionalConta = service.buscarId(id);
+        if(optionalConta.isPresent()) {
+            service.excluir(id);
+            return 	ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 
 }
--- src/main/java/com/banco/bluebank/service/ContaService.java
+++ src/main/java/com/banco/bluebank/service/ContaService.java
@@ -1,5 +1,40 @@
 package com.banco.bluebank.service;
 
-public interface ContaService {
+import com.banco.bluebank.model.Conta;
+import com.banco.bluebank.repository.ContaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ContaService {
+
+    @Autowired
+    private ContaRepository contaRepository;
+
+
+    public  List<Conta> buscarTodos() {
+        return contaRepository.findAll();
+    }
+
+    public Optional<Conta> buscarId(Long id){
+      return contaRepository.findById(id);
+    }
+
+   public Conta salvar(Conta contaInput){
+        return contaRepository.save(contaInput);    
+   }
+
+    public Conta buscarPorIdParaAtualizar(Long id) {
+        Optional<Conta> optionalConta = contaRepository.findById(id);
+        return optionalConta.orElseThrow(() -> new RuntimeException("Não foi possível encontrar essa com por esse ID!"));
+    }
+
+    @Transactional
+    public void excluir(Long id) {
+        contaRepository.deleteById(id);
+    }
 }
