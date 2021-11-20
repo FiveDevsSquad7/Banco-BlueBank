c5773498f83464c2fa408006612bd06162705699
carlosbetiol <33238620+carlosbetiol@users.noreply.github.com>
2021-11-20 09:12:17 -0300

    Revert "Adjusting Refactoring Exception"

--- src/main/java/com/banco/bluebank/exceptions/EntidadeNaoEncontradaException.java
+++ src/main/java/com/banco/bluebank/exceptions/EntidadeNaoEncontradaException.java
@@ -4,8 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public abstract class EntidadeNaoEncontradaException extends NegocioException 
-{
+public abstract class EntidadeNaoEncontradaException extends NegocioException {
 
 	private static final long serialVersionUID = 1L;
 
