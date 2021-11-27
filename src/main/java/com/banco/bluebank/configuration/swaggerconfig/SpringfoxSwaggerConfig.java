08e4fe8a11c6fb5b120295beb879879c335e4e41
Weslley Lima <weslleyclima@yahoo.com.br>
2021-11-27 16:33:15 -0300

    Ajuste à documentação ao Swagger como Modelo cujas alterações se devem fazer

--- src/main/java/com/banco/bluebank/configuration/swaggerconfig/SpringfoxSwaggerConfig.java
+++ src/main/java/com/banco/bluebank/configuration/swaggerconfig/SpringfoxSwaggerConfig.java
@@ -4,19 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
-/**
- * @name: SpringfoxSwaggerConfig
- * @categoria: Configuração
- * @description: Util para documentção
- * @author: Dev. Weslley Lima
- * @version: V1
- */
+import java.util.List;
+
 @Configuration
 @EnableSwagger2
 public class SpringfoxSwaggerConfig {
@@ -30,6 +27,67 @@ public class SpringfoxSwaggerConfig {
                 .build()
                 .apiInfo(moreInfo());
 
+    }
+
+
+    private List<ResponseMessage> responseMessageForGET()
+    {
+        return new ArrayList<ResponseMessage>() {{
+            add(new ResponseMessageBuilder()
+                    .code(500)
+                    .message("500 message")
+                    .responseModel(new ModelRef("Error"))
+                    .build());
+            add(new ResponseMessageBuilder()
+                    .code(403)
+                    .message("Forbidden!")
+                    .build());
+        }};
+    }
+
+    private List<ResponseMessage> responseMessageForPUT()
+    {
+        return new ArrayList<ResponseMessage>() {{
+            add(new ResponseMessageBuilder()
+                    .code(500)
+                    .message("500 message")
+                    .responseModel(new ModelRef("Error"))
+                    .build());
+            add(new ResponseMessageBuilder()
+                    .code(403)
+                    .message("Forbidden!")
+                    .build());
+        }};
+    }
+
+    private List<ResponseMessage> responseMessageForPOST()
+    {
+        return new ArrayList<ResponseMessage>() {{
+            add(new ResponseMessageBuilder()
+                    .code(500)
+                    .message("500 message")
+                    .responseModel(new ModelRef("Error"))
+                    .build());
+            add(new ResponseMessageBuilder()
+                    .code(403)
+                    .message("Forbidden!")
+                    .build());
+        }};
+    }
+
+    private List<ResponseMessage> responseMessageForDELETE()
+    {
+        return new ArrayList<ResponseMessage>() {{
+            add(new ResponseMessageBuilder()
+                    .code(500)
+                    .message("500 message")
+                    .responseModel(new ModelRef("Error"))
+                    .build());
+            add(new ResponseMessageBuilder()
+                    .code(403)
+                    .message("Forbidden!")
+                    .build());
+        }};
     }
 
     private ApiInfo moreInfo() {
