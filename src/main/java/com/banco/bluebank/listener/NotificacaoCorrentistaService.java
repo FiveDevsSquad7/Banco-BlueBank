23d08fe92a33c0385fa13df2e6f1b54ed076ae0f
Carlos Betiol <carlos@sistelpa.com>
2021-11-30 14:35:16 -0300

    notificacao por email e sms colocado com eventlistener de forma asyncrona e granular

--- src/main/java/com/banco/bluebank/listener/NotificacaoCorrentistaService.java
+++ src/main/java/com/banco/bluebank/listener/NotificacaoCorrentistaService.java
@@ -1,30 +1,56 @@
 package com.banco.bluebank.listener;
 
-import com.banco.bluebank.model.Correntista;
-import com.banco.bluebank.notificacao.Notificador;
+import com.banco.bluebank.notificacao.NotificadorEmail;
+import com.banco.bluebank.notificacao.NotificadorSMS;
 import com.banco.bluebank.service.MovimentacaoRealizadaEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 @Component
 public class NotificacaoCorrentistaService {
 
     @Autowired
-    private List<Notificador> notificadores;
+    private NotificadorEmail notificadorEmail;
 
+    @Autowired
+    private NotificadorSMS notificadorSMS;
+
+    @Async
     @EventListener
-    public void movimentacaoRealizadaListener(MovimentacaoRealizadaEvent event) {
+    public void enviarSmsContaDebitoMovimentacaoRealizadaListener(MovimentacaoRealizadaEvent event) {
 
-        for (Notificador notificador : notificadores) {
-            notificador.notificar(event.getMovimentacao().getContaDebito().getCorrentista(),
+        notificadorSMS.notificar(event.getMovimentacao().getContaDebito().getCorrentista(),
                     String.format("Sua conta acabou de sofrer uma movimentacao no valor de %f",event.getMovimentacao().getValor()));
-            notificador.notificar(event.getMovimentacao().getContaCredito().getCorrentista(),
-                    String.format("Sua conta acabou de sofrer uma movimentacao no valor de %f",event.getMovimentacao().getValor()));
-        }
 
     }
+
+    @Async
+    @EventListener
+    public void enviarSmsContaCreditoMovimentacaoRealizadaListener(MovimentacaoRealizadaEvent event) {
+
+        notificadorSMS.notificar(event.getMovimentacao().getContaCredito().getCorrentista(),
+                String.format("Sua conta acabou de sofrer uma movimentacao no valor de %f",event.getMovimentacao().getValor()));
+
+    }
+
+    @Async
+    @EventListener
+    public void enviarEmailContaDebitoMovimentacaoRealizadaListener(MovimentacaoRealizadaEvent event) {
+
+        notificadorEmail.notificar(event.getMovimentacao().getContaDebito().getCorrentista(),
+                String.format("Sua conta acabou de sofrer uma movimentacao no valor de %f",event.getMovimentacao().getValor()));
+
+    }
+
+    @Async
+    @EventListener
+    public void enviarEmailContaCreditoMovimentacaoRealizadaListener(MovimentacaoRealizadaEvent event) {
+
+        notificadorEmail.notificar(event.getMovimentacao().getContaCredito().getCorrentista(),
+                String.format("Sua conta acabou de sofrer uma movimentacao no valor de %f",event.getMovimentacao().getValor()));
+
+    }
+
 }
